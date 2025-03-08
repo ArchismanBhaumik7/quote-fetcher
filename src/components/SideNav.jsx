6936f6b14526859d9dfa29c77420a49c4e3398cd
ArchismanBhaumik7 <168325494+ArchismanBhaumik7@users.noreply.github.com>
2025-03-09 00:12:13 +0530

    Created Sidebar
    Created Sidebar, worked on its design and functionalities, added an svg item to serve as menu icon, and designed the Home Page

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,15 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createBrowserRouter, Link } from 'react-router-dom'
+import menuIcon from "../assets/menu.svg";
 
 const SideNav = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
+        <>
+            {/* Menu Icon (Top Left Corner) */}
+            <button
+                className="fixed top-4 left-4 z-50 p-2 focus:outline-none"
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <img src={menuIcon} alt="Menu" className="w-8 h-8" />
+            </button>
+
+            {/* Sidebar */}
+            <div
+                className={`fixed top-0 left-0 h-full w-[360px] bg-black text-white shadow-lg 
+                transform transition-transform duration-300 ease-in-out 
+                ${isOpen ? "translate-x-0" : "-translate-x-full"}`}
+            >
+
+                {/* Navigation Links */}
+                <nav className="mt-16 flex flex-col space-y-4 p-10">
+                    <Link
+                        to="/"
+                        className="p-4 rounded-3xl bg-gray-900 hover:bg-gray-800 transition"
+                        onClick={() => setIsOpen(false)}
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        to="/quotes"
+                        className="p-4 rounded-3xl bg-gray-900 hover:bg-gray-800 transition"
+                        onClick={() => setIsOpen(false)}
+                    >
+                        Quotes
+                    </Link>
+                </nav>
+            </div>
+        </>
+    );
+
+    /* return (
         <div>
             <nav>
                 <Link to='/'><li>Home</li></Link>
                 <Link to='/quotes'><li>Quotes</li></Link>
             </nav>
         </div>
-    )
+    ) */
 }
 
 export default SideNav
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      This is Home Page
+      <div className="hello text-4xl py-8">This is Home Page for the Quote Fetcher App.</div>
+      <div className="description text-2xl text-black p-6 rounded-4xl"
+      style={{ backgroundColor: "rgba(217, 149, 229, 0.65)" }}>
+      {/*Using custom RGBA values seems to not be supported in latest version of Tailwind CSS*/}
+        To access the main app, please navigate to the Quotes page using the Sidebar menu, 
+        which can be accessed via the menu button on the corner.↖️
+      </div>
+      <div className="have-fun text-2xl py-6">
+        Thank you and have fun!! 
+        <span className="emoji text-4xl">☺️</span>
+      </div>
     </div>
   )
 }
